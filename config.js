118575a20c857c6f996575cf34d22f259ffcafca
Jean-Paul Kouadio <115308301+JPK2001@users.noreply.github.com>
2026-02-13 23:35:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "DEBORAH A.K.A From Scratch",
+    valentineName: "DEBORAH... Cara Mia",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yayyy! I'm the luckiest guy in the worlddd! I don't deserve you... 🎉💝💖💝💓",
-        message: "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀HAPPY VALENTINE's DAY TO MY BABYGIRL, surprise incoming you know when.🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀",
+        message: "🚀🚀🚀HAPPY VALENTINE's DAY TO MY BABYGIRL, Hope you will love my surprise on Sunday.🚀🚀🚀",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
@@ -73,17 +73,15 @@ const CONFIG = {
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
-    // Music Settings
+    //Music Settings
     music: {
-        enabled: true, // Music feature is enabled
-        autoplay: true, // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dwtshskdx/video/upload/v1771042822/die-with-a-smile-official-music-video_erqzgz.mp3", // Paste your music URL here
-        startText: "🎵 Play Music", // Button text to start music
-        stopText: "🔇 Stop Music", // Button text to stop music
-        volume: 0.5 // Volume level (0.0 to 1.0)
-    }    
-
-
+        enabled: true,
+        autoplay: true,
+        musicUrl: "https://res.cloudinary.com/dwtshskdx/video/upload/v1771042822/die-with-a-smile-official-music-video_erqzgz.mp3",
+        startText: "🎵 Play Music",
+        stioText: "🔇 Stop Music",
+        volume: 1
+    }
 };
 
 // Don't modify anything below this line unless you know what you're doing
